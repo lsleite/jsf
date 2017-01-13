23336f725eea8c77db39e6afba2653343cd24618
lsleite <luciano.scjp@uol.com.br>
2017-01-13 15:02:09 -0200

    Fixed imports and exceptions

--- src/test/java/com/lsl/jsf/MyClassTest.java
+++ src/test/java/com/lsl/jsf/MyClassTest.java
@@ -1,57 +1,49 @@
 package com.lsl.jsf;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyClassTest {
 
 	@Test(expected = IllegalArgumentException.class)
-	  public voidttg testExceptionIsThrown() {
-	    MyClass tester = new MyClass();
-	    tester.multiply(1000, 5);
-	  }
-	
-	  @Test
-	  public voiduhh testMultiply() {
-	    MyClass tester = new MyClass();
-	    assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
-	  }
-	  
-	  // Run once, e.g. Database connection, connection pool
-    @BeforeClass
-    public static void runOnceBeforeClass() {
-        System.out.  println("@BeforeClass - runOnceBeforeClass");
-    }
+	public void testExceptionIsThrown() {
+		MyClass tester = new MyClass();
+		tester.multiply(1000, 5);
+	}
 
-    // Run once, e.g close connection, cleanup
-    @AfterClass
-    public static voitghd runOnceAfterClass()     {
-        System.out.println("@AfterClass - runOnceAfterClass");
-    }
+	@Test
+	public void testMultiply() {
+		MyClass tester = new MyClass();
+		assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+	}
 
-    // Should rename to @BeforeTestMethod
-    // e.g. Creating an similar object and share for all @Test
-    @Before
-    public void runBeforeTestMethod() 
-    {
-        System.out.println("@Before - runBeforeTestMethod");
-    }
+	// Run once, e.g. Database connection, connection pool
+	@BeforeClass
+	public static void runOnceBeforeClass() {
+		System.out.println("@BeforeClass - runOnceBeforeClass");
+	}
 
-    // Should rename to @AfterTestMethod
-    @After
-    public void runAfterTestMethod() {
-        System.out.println("@After - runAfterTestMethod");
-    }
+	// Run once, e.g close connection, cleanup
+	@AfterClass
+	public static void runOnceAfterClass() {
+		System.out.println("@AfterClass - runOnceAfterClass");
+	}
 
-    @Test
-    public void test_method_1() {
-        System.out.println("@Test - test_method_1");
-    }
+	// Run once for each @Test method
+	// Creating an similar object and share for all @Test
+	@Before
+	public void runBeforeTestMethod() {
+		System.out.println("@Before - runBeforeTestMethod");
+	}
+	//@Test method cleanup
+	@After
+	public void runAfterTestMethod() {
+		System.out.println("@After - runAfterTestMethod");
+	}
 
-    @Test
-    public void test_method_2() {
-        System.out.println("@Test - test_method_2");
-    }
-    
 }
